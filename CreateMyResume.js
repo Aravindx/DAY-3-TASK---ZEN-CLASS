2eeb8050d9eafb8ad177e6c3b300c3fd32ead55c
Aravind M <109063587+Aravindx@users.noreply.github.com>
2022-10-15 11:02:22 +0530

    Create CreateMyResume.js

---
+++ CreateMyResume.js
@@ -0,0 +1,52 @@
+let myResume={
+    "basics": {
+      "name": "ARAVIND.M",
+      "email": "aravindmathi517@gmail.cpm",
+      "phone": 8608167337,
+      "degree": "B.E (BIOMEDICAL ENGINEERING)",
+      "location": {
+        "address": "46,PUSHPAVANAM",
+        "postalCode": 614809,
+        "city": "NAGAPATTINAM",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/aravind-m-350b5821b",
+          "github":"https://github.com/Aravindx"
+        }
+      ]
+    },
+   
+    "education": [
+      {
+        "institution": "RAJALAKSHMI ENGINEERING COLLEGE",
+        "department": "BIOMEDICAL",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2022,
+        "gpa": 7.7,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript,C#",
+        "level": "beginer",
+        "project": [
+          "BANK APPLICATION USING C#"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "MUSIC,TRAVEL,TECH,SPORTS,FITNESS",
+      }
+    ]
+  }
+  console.log(myResume);
